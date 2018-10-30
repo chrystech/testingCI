a7157bca3f54c0717c36712ed69dea4304f73adc
Nova Reaper <chrystechsystems@gmail.com>
2018-10-30 18:39:41 -0500

    test tags?

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,11 @@
 var expect = require('chai').expect;
 
+console.log('##teamcity[testSuiteStarted name=\'tests\']');
+console.log('##teamcity[testStarted name=\'test1\']');
 describe('Test1',function(){
    it('should add 2+2=4', function(){
        expect(2+2).to.be.equal(4);
    })
 });
+console.log('##teamcity[testFinished name=\'test1\']');
+console.log("##teamcity[testSuiteFinished name='tests']");
