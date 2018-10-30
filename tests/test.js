60d3cd3dbaf54d952ccaa7786ced0ce11538157c
Nova Reaper <chrystechsystems@gmail.com>
2018-10-30 18:48:59 -0500

    Hoping for a failed test

--- tests/test.js
+++ tests/test.js
@@ -9,10 +9,15 @@ describe('Test1',function(){
 });
 console.log('##teamcity[testFinished name=\'test1\']');
 console.log('##teamcity[testStarted name=\'test2\']');
-describe('Test1',function(){
-    it('should add 2+2=4', function(){
-        expect(2+5).to.be.equal(4);
-    })
-});
+try {
+    describe('Test1', function () {
+        it('should add 2+2=4', function () {
+            expect(2 + 5).to.be.equal(4);
+        })
+    });
+} catch(e){
+    console.log('##teamcity[testFailed name=\'System can send an email to postmark to be delivered to a user\' message=\'" + e.message + "\' details=\'" + e + "\']')
+}
+
 console.log('##teamcity[testFinished name=\'test2\']');
 console.log("##teamcity[testSuiteFinished name='tests']");
