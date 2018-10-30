1bfd08240bb511fa6c4615bea1cbdc4c70be7fd9
Nova Reaper <chrystechsystems@gmail.com>
2018-10-30 18:45:23 -0500

    Hoping for a failed test

--- tests/test.js
+++ tests/test.js
@@ -8,4 +8,11 @@ describe('Test1',function(){
    })
 });
 console.log('##teamcity[testFinished name=\'test1\']');
+console.log('##teamcity[testStarted name=\'test2\']');
+describe('Test1',function(){
+    it('should add 2+2=4', function(){
+        expect(2+5).to.be.equal(4);
+    })
+});
+console.log('##teamcity[testFinished name=\'test2\']');
 console.log("##teamcity[testSuiteFinished name='tests']");
